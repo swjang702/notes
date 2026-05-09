ba1035827b877442b2e8748989c268debf89d3de
Sunwoo Jang <swjang702@gmail.com>
2026-05-09 17:22:06 +0900

    Quartz sync: May 9, 2026, 5:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,10 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        //header: "Schibsted Grotesk",
+        //body: "Source Sans Pro",
+        header: "Alegreya Sans",
+        body: "Alegreya Sans",
         code: "IBM Plex Mono",
       },
       colors: {
